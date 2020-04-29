59830e91ba687395441d798173e9a76b4b9d6799
Jesse Tao <c.tao@griffith.edu.au>
2020-04-29 22:10:19 +1000

    npm install --save-dev @ckeditor/ckeditor5-alignment

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,18 @@ class App extends Component {
                 <h2>Using CKEditor 5 build in React</h2>
                 <CKEditor
                     editor={ ClassicEditor }
-                    data="<p>Hello from CKEditor 5!</p>"
+                    config={{
+                        toolbar: [ 'heading', '|', 'alignment', '|', 'italic', 'bold', 'bulletedList', 'numberedList', '|', 'undo', 'redo' ],
+                        heading: {
+                            options: [
+                                { model: 'heading2', view: 'h2', title: 'Heading', class: 'ck-heading_heading2' },
+                                { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' }
+                            ]
+                        },
+                        placeholder: "Enter your response here"
+                    }}
+
+                    // data="<p>Hello from CKEditor 5!</p>"
                     onInit={ editor => {
                         // You can store the "editor" and use when it is needed.
                         console.log( 'Editor is ready to use!', editor );
